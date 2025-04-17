fab809367ee3992f237077322a28f3dadc8f31a7
Ricky Harefa <114171685+ricky-hrf@users.noreply.github.com>
2025-04-17 22:20:01 +0700

    memperbaiki komponen produk

---
+++ src/components/Fragments/Products.jsx
@@ -0,0 +1,20 @@
+import PropTypes from "prop-types";
+import ProductCard from "../Elements/ProductCard/products";
+
+const Products = ({ products }) => {
+
+  return (
+    <div className="container mx-auto px-6">
+          <h2 className="text-lg md:xl lg:2xl font-bold text-left text-gray-800 mb-4">Produk Kami</h2>
+          <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-4 justify-center">
+            {products.map((product) => (
+                  <ProductCard key={product.id} product={product} />
+            ))}
+            </div>
+        </div>
+  )
+}
+Products.propTypes = {
+  products: PropTypes.array.isRequired,
+};
+export default Products;
